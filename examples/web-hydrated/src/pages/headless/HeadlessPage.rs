5c6c4564a1b5793b469d62f3d082073ee7b68211
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-10-25 23:49:04 +0300

    style: color and padding in SidebarNav, HeadlessPage
    Change sidebar nav background and padding
    
    Change of HeadlessPage main component paddings
    
    Change one of the scanning folder paths, in tailwind config
    
    Closes #58

--- examples/web-hydrated/src/pages/headless/HeadlessPage.rs
+++ examples/web-hydrated/src/pages/headless/HeadlessPage.rs
@@ -13,7 +13,7 @@ pub fn HeadlessPage(cx: Scope) -> Element {
             class: "flex",
             SideBarNav {},
             main {
-                class: "grow relative p-4 h-auto text-xl lg:ml-72 lg:w-full lg:h-full leading-normal",
+                class: "grow relative px-24 pt-12 pb-6 text-xl lg:ml-72 lg:w-full lg:h-full leading-normal",
                 Outlet::<Route> {}
             },
         }
