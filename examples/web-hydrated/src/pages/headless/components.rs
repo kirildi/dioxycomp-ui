8f752f480b8fb9aee81f6066a0672cfc8a7845cd
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-11-08 20:49:43 +0200

    refactor: work on examples directory
    Clean examples directory
    Move web-hydrated to a new repository
    Remove generate-pages
    workflow, as not intended
    current project
    
    Closes #80

--- examples/web-hydrated/src/pages/headless/components.rs
+++
@@ -1,6 +0,0 @@
-#![allow(non_snake_case)]
-pub mod ButtonPage;
-pub mod CheckboxPage;
-pub mod RadioPage;
-pub mod SelectPage;
-pub mod SideBarNav;
--- examples/web/src/pages/headless.rs
+++
@@ -1,4 +0,0 @@
-#![allow(non_snake_case)]
-
-pub mod HeadlessPage;
-pub mod components;
